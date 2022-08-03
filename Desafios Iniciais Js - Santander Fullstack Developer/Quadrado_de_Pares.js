cd4c84259bdda54d887d5cc1276fea7cdf01dbc3
Jéssika Ferreira <93353985+jessikaraw@users.noreply.github.com>
2022-08-03 11:40:15 -0300

    Add files via upload

---
+++ Desafios Iniciais Js - Santander Fullstack Developer/Quadrado_de_Pares.js
@@ -0,0 +1,23 @@
+Leia um valor inteiro N. Apresente o quadrado de cada um dos valores pares, de 1 até N, inclusive N, se for o caso.
+
+Entrada
+A entrada contém um valor inteiro N (5 < N < 2000).
+
+Saída
+Imprima o quadrado de cada um dos valores pares, de 1 até N, conforme o exemplo abaixo.
+
+Tome cuidado! Algumas linguagens tem por padrão apresentarem como saída 1e+006 ao invés de 1000000 o que ocasionará resposta errada. 
+Neste caso, configure a precisão adequadamente para que isso não ocorra.
+
+// a função gets() é implementada dentro do sistema para ler as entradas(inputs) dos dados e a função print() para imprimir a saída (output) de dados e já pula uma linha ("\n")
+// Abaixo segue um exemplo de código que você pode ou não utilizar
+let lines = gets().split('\n');
+var N = parseInt(lines.shift());
+
+for (let i = 1; i <= N; i++) {
+
+//TODO: Complete os espaços em branco com uma possível solução para o desafio
+
+	if (i % 2 === 0) 
+          console.log(`${i}^2 = ${i**2}`);
+}
