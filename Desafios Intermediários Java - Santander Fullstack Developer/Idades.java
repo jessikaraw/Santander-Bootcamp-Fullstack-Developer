ba64be452a7b7383a728d55503a4d3a417a49070
Jéssika Ferreira <93353985+jessikaraw@users.noreply.github.com>
2022-08-07 22:03:41 -0300

    Add files via upload

---
+++ Desafios Intermediários Java - Santander Fullstack Developer/Idades.java
@@ -0,0 +1,28 @@
+/*
+Faça um algoritmo para ler um número indeterminado de dados, contendo cada um, a idade de um indivíduo. 
+O último dado, que não entrará nos cálculos, contém o valor de idade negativa. Calcular e imprimir a idade média deste grupo de indivíduos. 
+*/
+
+// Abaixo segue um exemplo de código que você pode ou não utilizar
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Idades {
+
+    public static void main(String[] args) throws IOException {
+        Scanner leitor = new Scanner(System.in);
+        int cont = 0;
+        int n = 0;
+        double soma = 0;
+        do {
+            n = leitor.nextInt();
+            if (n > 0) {
+                soma = soma + n;
+                cont = cont+1;
+            }
+        } while (n > 0);
+        double media = soma / cont;
+        System.out.println(String.format("%.2f", media));
+    }
+
+}
---
+++ Desafios Intermediários Java - Santander Fullstack Developer/dbw.java
@@ -0,0 +1,38 @@
+/*
+Todos os habitantes do Mundo Mágico estão super animados com a abertura do Dragão Berrador World, o mais novo parque de diversões do universo bruxo. 
+Infelizmente foram impostas algumas restrições no momento da homologação do brinquedo pelo Ministério Bruxo. Por questões de segurança, há uma altura mínima e uma altura máxima que as pessoam devem ter para poder passear na montanha-russa.
+Para o dia da inauguração do parque, todos os convidados realizaram um pré-cadastro no qual indicaram sua altura. Para reduzir filas e otimizar a operação do parque no primeiro dia, você foi contratado pela gerência do Dragão Berrador World para fazer um programa que, dado o número de visitantes, altura mínima, altura máxima e as alturas de todos os visitantes, calcule quantas pessoas poderão andar na montanha-russa. 
+*/
+
+// Abaixo segue um exemplo de código que você pode ou não utilizar
+import java.io.IOException;
+import java.util.Scanner;
+public class DIO {
+
+	public static void main(String[] args) throws IOException {
+		Scanner leitor = new Scanner(System.in);
+		while (leitor.hasNext()) {
+			int N = leitor.nextInt();
+			int min = leitor.nextInt();
+			int max = leitor.nextInt();
+			int cont = 0;
+
+// TODO: complete os espaços em branco com sua solução para o problema
+
+for (int i = 0; i < N; i++) {
+
+int A = leitor.nextInt();
+
+if (A >= min && A <= max)
+
+cont++;
+
+}
+
+System.out.println(cont);
+
+}
+
+}
+
+}
